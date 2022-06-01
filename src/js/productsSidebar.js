2ff36e64d56c782280ca20f9dba660901ebc373c
Artem Safin <artemsafin.webdev@gmail.com>
2022-06-01 16:38:12 +0300

    Правки

--- src/js/productsSidebar.js
+++ src/js/productsSidebar.js
@@ -4,20 +4,37 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 export default function productsSidebar() {
+   
     ScrollTrigger.matchMedia({
         '(min-width: 641px)': function() {
             const products = document.querySelector('.products');
-            const sidebar = document.querySelector('.products__sidebar');
+            const scrollWrapper = document.querySelector('.products__main-scroll-wrapper');
+            const mainInner = document.querySelector('.products__main-inner');
 
-            if (!products || !sidebar) return;
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: products,
+                    start: 'bottom bottom',
 
-            ScrollTrigger.create({
-                trigger: sidebar,
-                start: 'top top+=40',
-                endTrigger: products,
-                end: 'bottom bottom',
-                pin: sidebar,
-                pinSpacing: false
+                    end: () => mainInner.offsetHeight - scrollWrapper.offsetHeight,
+                    pin: '.products__content',
+                    pinSpacing: true,
+                    scrub: true
+                }
+            });
+
+            tl.to(mainInner, {
+                y: () => {
+                    const distance = mainInner.offsetHeight - scrollWrapper.offsetHeight;
+                    if (distance > 0) {
+                        return -1 * (mainInner.offsetHeight - scrollWrapper.offsetHeight);
+                    } else {
+                        return 0;
+                    }
+                   
+                },
+                duration: 1,
+                ease: 'none'
             });
         }
     });
